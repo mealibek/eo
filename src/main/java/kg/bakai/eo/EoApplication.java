8687a055b26bbab706a3b06c3a7495185ca812bb
mealibek <mealibek@hotmail.com>
2024-01-27 15:21:01 +0600

    Initial commit

---
+++ src/main/java/kg/bakai/eo/EoApplication.java
@@ -0,0 +1,13 @@
+package kg.bakai.eo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/bakai/eo/EoApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.bakai.eo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
