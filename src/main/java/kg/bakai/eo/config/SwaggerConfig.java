080c41fd41ddd2aeaf0ba1efe96f8980fd50ab7d
mealibek <mealibek@hotmail.com>
2024-01-29 12:00:49 +0600

    added swagger-ui

---
+++ src/main/java/kg/bakai/eo/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package kg.bakai.eo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Bilingual API Documentation", version = "1.0", description = "API documentation for Bilingual application"))
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI();
+    }
+}
+
